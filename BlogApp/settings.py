53b000f53cb6d1b2748cb6edc5df6494564d95a3
medhavi <medhavi.srivastava16@gmail.com>
2020-07-29 19:33:26 +0530

    update

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i+1=w7fjacrbk@-a!#kb8gf9cf!z)-s3e*&6#0fv!i-8k30i04'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['blog-app-django-16.herokuapp']
+ALLOWED_HOSTS = ['blog-app-django-16.herokuapp.com']
 
 
 # Application definition
